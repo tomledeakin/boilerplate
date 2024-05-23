a61a02481a7c06a838868a905cc896089e08b612
tomledeakin <139051959+tomledeakin@users.noreply.github.com>
2024-05-24 02:00:50 +1000

    Start your next react project in seconds

--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -10,7 +10,7 @@ export const scope = 'boilerplate.containers.HomePage';
 export default defineMessages({
   startProjectHeader: {
     id: `${scope}.start_project.header`,
-    defaultMessage: 'Start your next react project in seconds seconds',
+    defaultMessage: 'Start',
   },
   startProjectMessage: {
     id: `${scope}.start_project.message`,
